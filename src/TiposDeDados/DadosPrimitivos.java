25014ab87690da5dd86956e09b66f5c61fedd6dd
Paulojoserc <pjrc83@gmail.com>
2025-01-27 14:41:32 -0300

    Dados Primitivos

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+         *Dados primitivos - int, double, float, char, boolean, short.
+         *Objetivo da aula: Criar um ninja - Naruto -
+         */
+
+        int idade = 16; // Valor maximo: 2 147 483 647
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = false;
+        long saldoBancario = 999999; // Valor maximo: 9.223.372.036.854.775.807
+
+    }
+}
