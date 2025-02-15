47ce2a90aeb8ee03a10e8735efb43037114e1e1b
Paulojoserc <pjrc83@gmail.com>
2025-02-15 16:57:40 -0300

    Aula de condicional do cursor biblia do java (fiasco)

---
+++ src/Condicoes/IfEElse.java
@@ -0,0 +1,28 @@
+package Condicoes;
+
+public class IfEElse {
+    public static void main(String[] args) {
+        /*
+         * if e else - (Condiçoes) { resultado}
+         * else if - (Condiçoes) { resultado}
+         * else - { resultado caso nada seja verdadeiro}
+         * Objetivo: Passar o ninja de nivel de acordo com o numero de missões
+         **/
+        //Ninja Naruto
+        String nome = "Naruto Uzumaki";
+        String rank;
+
+        int idade = 16;
+        boolean hokage = false;
+        short numeroDeMissoes = 20;
+
+        //se (condição {faça isso}
+        if (numeroDeMissoes == 10 && idade > 15) {
+            System.out.println("Rank: Chunnin");
+        } else if (numeroDeMissoes >=20) {
+            System.out.println("Rank: Jounin");
+        }else {
+            System.out.println("Rack: Gennim");
+        }
+    }
+}
