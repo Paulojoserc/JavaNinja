a5ded3385a978d5d015c96253e9d95e38b9740d1
Paulojoserc <pjrc83@gmail.com>
2025-02-15 16:26:08 -0300

    Aula primitivos e não primitivos do cursor biblia do java (fiasco)

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,20 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+    //Tudo que for digitado aqui dentro com comando PSVM vai se compilado pelo Java
 
+        /*
+        Dados não primitivos: String, Array, Class, enum
+        Objetivo: Criar um ninja, e atribuir metodos a ele
+         */
+
+        String nome = "Naruto Uzumaki";
+        String nomeEmCaixaAlta= nome.toUpperCase(); //ToUppercase vai colocar tudo em CASPLOCK
+        System.out.println("Esse texto esta em CAPSLOCK "+nomeEmCaixaAlta);
+        System.out.println("Este texto esta normal "+ nome);
+
+        String aldeia = "Aldeia da Folha";
+        String aldeiaEmCaixaBaixa = aldeia.toLowerCase(); // ToLoweCase vai colocar tudo em caixa baixa/minusculo
+        System.out.println(aldeiaEmCaixaBaixa);
     }
 }
