01ee904c11d2effb8a39b0166c95fc8e786fe4a9
Paulo Costa <pjrc83@gmail.com>
2025-07-15 23:35:52 -0300

    Estrutura do ScannerDoUsuario

---
+++ src/Condicoes/ScannerDoUsuario.java
@@ -0,0 +1,8 @@
+package Condicoes;
+
+public class ScannerDoUsuario {
+    /*
+        Scanner = É jeito de trazer o usuario para dentro da aplicação.
+        Objetivo: O usuario vai criar um ninja e vamos validar os dados
+     */
+}
