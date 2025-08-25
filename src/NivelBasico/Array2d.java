15c0a072217f86f2efeea91ade73780f6b7959cf
Paulojoserc <pjrc83@gmail.com>
2025-08-25 14:37:47 -0300

    Aula Matiz

---
+++ src/NivelBasico/Array2d.java
@@ -0,0 +1,31 @@
+package NivelBasico;
+
+public class Array2d {
+    public static void main(String[] args) {
+        String[][] ninjasEAldeias = new String[3][3];
+
+        ninjasEAldeias[0][0] = "Folha";
+        ninjasEAldeias[0][1] = "Naruto Uzumaki";
+        ninjasEAldeias[0][2] = "Sasuke Uchiha";
+
+        ninjasEAldeias[1][0] = "Nevoa";
+        ninjasEAldeias[1][1] = "Zubuza";
+        ninjasEAldeias[1][2] = "Haku";
+
+        ninjasEAldeias[2][0] = "Deserto";
+        ninjasEAldeias[2][1] = "Gaara";
+        ninjasEAldeias[2][2] = "Temari";
+
+        for (int i = 0; i < ninjasEAldeias.length; i++) {
+            System.out.println("Aldeia: " + ninjasEAldeias[i][0]);
+
+            for (int j = 1; j < ninjasEAldeias[i].length; j++) {
+                System.out.print(" Ninja: " + ninjasEAldeias[i][j]+", ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
+
