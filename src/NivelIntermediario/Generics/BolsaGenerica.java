dfe56aca825260b021dd1ef78878536101f77d47
Paulojoserc <pjrc83@gmail.com>
2026-03-18 18:39:27 -0300

    Aula de Não Generics Part1

---
+++ src/NivelIntermediario/Generics/BolsaGenerica.java
@@ -0,0 +1,35 @@
+package NivelIntermediario.Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BolsaGenerica <T>{
+    private List<T> equipamentos;
+
+    public BolsaGenerica() {
+        this.equipamentos = new ArrayList<>();
+    }
+
+
+    //Colocar equipamentos Genéricos
+    public void adcionarEquipamentos(T equipamentoGenerico){
+        this.equipamentos.add(equipamentoGenerico);
+    }
+
+    public List<T> getEquipamentos() {
+
+        return equipamentos;
+    }
+
+    public void setEquipamentos(List<T> equipamentos) {
+
+        this.equipamentos = equipamentos;
+    }
+
+    @Override
+    public String toString() {
+        return "BolsaGenerica{" +
+                "equipamentos=" + equipamentos.toString() +
+                '}';
+    }
+}
